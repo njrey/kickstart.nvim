db541a729caecc77c86a5070242dd8a964dd26d6
Nolan Reynolds <nolan.reynolds@nokia.com>
2024-03-29 15:39:29 -0400

    Add harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,24 @@
+return {
+  { -- Harpoon
+    'ThePrimeagen/harpoon',
+    dependencies = { 'nvim-lua/plenary.nvim', 'telescope.nvim' },
+    branch = 'harpoon2',
+
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():append()
+      end)
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end)
+    end,
+  },
+}
